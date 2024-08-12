4783028bc9feb962fdb6bae62de7b7785acb9fc8
Kieran Wall <wer2ct@virginia.edu>
2024-08-12 12:54:18 -0400

    added link functionality

--- dm_skimmer.py
+++ dm_skimmer.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 
 def skimmer(address,tag):
+    print("-------------------------------------")
     print(f'Fetching relevant entries from {tag}:')
     
     #grabbing webpage
@@ -14,8 +15,12 @@ def skimmer(address,tag):
     abstracts = soup.find_all('p', class_ = "mathjax")
     titles = soup.find_all('div',class_="list-title mathjax")
 
+    links = soup.css.select('a[id^="pdf-"]')
+
+    
     abstracts_list = []
     titles_list = []
+    links_list = []
 
     keyword_list = [['dark',10],['matter',10]]
     #set score threshold at minimum of one instance of each keyword (25 atm)
@@ -27,6 +32,8 @@ def skimmer(address,tag):
     for title in titles:
         #print(title.contents[1])
         titles_list.append(str(title.contents[1]))
+    for link in links:
+        links_list.append(('https://arxiv.org' + str(link).split('"')[3]))
 
     for i in range(len(abstracts_list)):
         abs_word_list = abstracts_list[i].split(" ")
@@ -35,6 +42,8 @@ def skimmer(address,tag):
         del abs_word_list[(len(abs_word_list) - 8):len(abs_word_list)]
         del title_word_list[0:10]
         del title_word_list[(len(title_word_list) - 8):len(title_word_list)]
+        for i in range(len(titles_list)):
+            titles_list[i] = titles_list[i].strip()
         score = 0
         for keyword in keyword_list:
             for word in abs_word_list:
@@ -42,13 +51,13 @@ def skimmer(address,tag):
                     score += 1*keyword[1]
         if True == True: #placeholder for additional filter
             if score >= 20:
-                print(f'{titles_list[i]} -> score  = {score}\n')
+                print(f'\n{titles_list[i]} \nscore  = {score} \nlink = {links_list[i]}\n')
 
 def url_merge(tag):
     return('https://arxiv.org/list/'+ f'{tag}/new')
 
 def main():
-    tags = ['hep-ex','hep-th']
+    tags = ['hep-ex', 'hep-th']
     for tag in tags:
         skimmer((url_merge(tag)),tag)
 
